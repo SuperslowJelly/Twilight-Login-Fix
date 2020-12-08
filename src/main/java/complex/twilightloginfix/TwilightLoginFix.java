f825979131dae37040343ca296b6bc25e4cbfee6
Chae Taylor <chaetaylor@msn.com>
2020-12-08 21:59:32 +0000

    Swapped to Nucleus integration for spawn finding

--- src/main/java/complex/twilightloginfix/TwilightLoginFix.java
+++ src/main/java/complex/twilightloginfix/TwilightLoginFix.java
@@ -2,13 +2,18 @@ package complex.twilightloginfix;
 
 import com.google.common.base.Objects;
 import com.google.inject.Inject;
+import io.github.nucleuspowered.nucleus.api.NucleusAPI;
+import io.github.nucleuspowered.nucleus.api.nucleusdata.Warp;
+import io.github.nucleuspowered.nucleus.api.service.NucleusWarpService;
 import org.slf4j.Logger;
 import org.spongepowered.api.Sponge;
 import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.event.Listener;
+import org.spongepowered.api.event.filter.cause.First;
 import org.spongepowered.api.event.game.state.GameStartedServerEvent;
 import org.spongepowered.api.event.network.ClientConnectionEvent;
 import org.spongepowered.api.plugin.Plugin;
+import org.spongepowered.api.world.Location;
 import org.spongepowered.api.world.World;
 
 @Plugin(
@@ -31,14 +36,22 @@ public class TwilightLoginFix {
     }
 
     @Listener
-    public void onPlayerLeave(ClientConnectionEvent.Disconnect event) {
-        Player player = event.getTargetEntity();
+    public void onPlayerLeave(ClientConnectionEvent.Disconnect event, @First Player player) {
         String worldName = player.getWorld().getName();
         if (Objects.equal(worldName, "DIM7") || (Objects.equal(worldName, "DIM-777") || (Objects.equal(worldName, "DIM5")))) {
-            World world;
-            if (!Objects.equal( world = Sponge.getServer().getWorld("world").orElse(null), null)) {
-                player.setLocation(world.getSpawnLocation());
-                logger.info("Player, " + player.getName() + ", logged out in the Twilight Forest: Re-locating to spawn...");
+            NucleusWarpService warpService;
+            if (!Objects.equal(warpService = NucleusAPI.getWarpService().orElse(null), null)) {
+                Warp spawn;
+                if (!Objects.equal(spawn = warpService.getWarp("spawn").orElse(null), null)) {
+                    Location location;
+                    if (!Objects.equal(location = spawn.getLocation().orElse(null), null)) {
+                        World world;
+                        if (!Objects.equal(world = Sponge.getServer().getWorld(location.getExtent().getUniqueId()).orElse(null), null)) {
+                            player.setLocation(world.getSpawnLocation());
+                            logger.info("Player, " + player.getName() + ", logged out in the Twilight Forest: Re-locating to spawn...");
+                        }
+                    }
+                }
             }
         }
     }
